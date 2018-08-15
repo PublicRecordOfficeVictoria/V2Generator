809ace446418fd3bdadf6c90cd5917a088d67926
andrew-waugh <38519724+andrew-waugh@users.noreply.github.com>
2018-08-15 15:15:25 +1000

    Added V2Creator.bat

--- VEOCreator.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set versclasspath="G:PROV\TECHNOLOGY MANAGEMENT\Application Development\VERS"
-java -classpath %versclasspath% VEOToolkitV2.VEOGenerator.VEOCreator -t %versclasspath%\VEOToolkitV2\VEOGenerator\encDirectory %*
